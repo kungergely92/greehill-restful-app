323fc8cbf12a5d5bbb4235a82d7f19ab9c21dc18
kungergely92 <kun.gergely.1992@gmail.com>
2021-05-29 21:41:33 +0200

    Using psutils to get cpu usage

--- HWSerial.py
+++ HWSerial.py
@@ -1,5 +1,6 @@
 import flask
 from datetime import datetime
+import psutil
 import json
 import os
 
@@ -9,20 +10,13 @@ class HWSerial(dict):
 
 app = flask.Flask(__name__)
 
-def read_proc_stat(proc_stat_path):
-    with open(proc_stat_path) as file:
-        entries = [float(column) for column in file.readline().strip().split()[1:]]
-    idle, sum = entries[3], sum(entries)
-    utilisation = 100.0 * (1.0 - idle / sum)
-    return str(utilisation)
-
 @app.route('/api/v1/time', methods=['GET'])
 def get_time_in_utc():
     return str(datetime.utcnow())
 
 @app.route('/api/v1/status', methods=['GET'])
 def get_cpu_uti():
-    return read_proc_stat('/proc/stat')
+    return f'{psutil.cpu_percent(interval=1)} %'
 
 @app.route('/api/v1/serializer/<param>', methods=['GET'])
 def get_serialized_object(param):
